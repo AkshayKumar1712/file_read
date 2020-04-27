fb72be9394d27936da1d46e1038a029d2b76c015
akshay <kotagiriakshay96@gmail.com>
2020-04-27 10:35:37 +0530

    read_from_private_folder_of_service

--- app/src/main/java/com/example/temp/MainActivity.java
+++ app/src/main/java/com/example/temp/MainActivity.java
@@ -12,12 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this,new String[]{"android.permission.WRITE_EXTERNAL_STORAGE","android.permission.READ__EXTERNAL_STORAGE"},200);
-        Context context=getApplicationContext();
-        Intent intent=new Intent(this,MyService.class);
-        intent.putExtra("path_of_the_file","/storage/emulated/0/SMS Creator Reports/input_report.txt");
-        startForegroundService(intent);
     }
 }
--- app/src/main/java/com/example/temp/MyService.java
+++ app/src/main/java/com/example/temp/MyService.java
@@ -1,10 +1,17 @@
 package com.example.temp;
 
+import android.Manifest;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.permissioneverywhere.PermissionEverywhere;
+import com.permissioneverywhere.PermissionResponse;
+import com.permissioneverywhere.PermissionResultCallback;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,12 +20,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String file_path=intent.getStringExtra("path_of_the_file");
+
         try {
+            String file_path=intent.getStringExtra("path_of_file");
             loadResultFromTxt(file_path);
         } catch (IOException e) {
             Log.d("TAG", "onCreate: "+e);
@@ -32,12 +41,11 @@ public class MyService extends Service {
         return null;
     }
 
-    private void loadResultFromTxt(String path) throws IOException {
+    public void loadResultFromTxt(String path) throws IOException {
 
-
+        Log.d("TAG", "loadResultFromTxt: "+getExternalFilesDir(null).getAbsolutePath());
 //        InputStream is=context.getContentResolver().openInputStream(Uri.fromFile(result_file));
-        File result_file=new File(path);
-
+        File result_file=new File("/storage/emulated/0/Android/data/com.example.temp/files/input_report.txt");
         FileInputStream fis=new FileInputStream(result_file);
 
         InputStreamReader isr = new InputStreamReader(fis);
@@ -47,5 +55,6 @@ public class MyService extends Service {
             String[] line_split=line.split(": ",2);
             Log.d("TAG", "loadResultFromTxt: "+line);
         }
+        fis.close();
     }
 }
